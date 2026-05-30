f677981dfc98394fdb00698005055b36b8c75d27
gojo96880-cloud <gojo96880@gmail.com>
2026-05-30 16:23:34 +0200

    Refactor main.go to implement Threat Detection Service
    Removed User, Token, and AuditLog structs. Added main function to start the Threat Detection Service.

---
+++ backend/threat-detection/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Println("Starting Threat Detection Service")
+	fmt.Println("Threat Detection Service running on port 8002")
+	select {}
+}
